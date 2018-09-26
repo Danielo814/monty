01cdc49648ebd565c551845aa215bd2b328fa56e
Daniel Ojeda <211@holbertonschool.com>
2018-09-26 22:19:59 +0000

    get functions

---
+++ get_func.c
@@ -0,0 +1,34 @@
+#include "monty.h"
+/**
+ * get_func - checks string and executes the appropriate function
+ * @command: command to execute
+ * @stack: pointer to stack
+ * @linenum: line number of command
+ */
+void get_func(char *command, stack_t **stack, unsigned int linenum)
+{
+	unsigned int i = 0;
+	instruction_t commands[] = {
+		{"push", push}, {"pall", pall},
+	     /**
+	      *	{"pop", pop}, {"pint", pint},
+	      *	{"swap", swap}, {"add", add},
+	      *	{"nop", nop},
+	      */
+		{NULL, NULL}
+	};
+	while (commands[i].opcode != NULL)
+	{
+		if (strcmp(commands[i].opcode, command) == 0)
+		{
+			commands[i].f(stack, linenum);
+			return;
+		}
+		i++;
+	}
+	if (commands[i].opcode == NULL)
+	{
+		printf("L%d: unknown instruction %s\n", linenum, command);
+		exit(EXIT_FAILURE);
+	}
+}
