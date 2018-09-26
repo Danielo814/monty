28e0797f141583bd376d9dc0eb03ac92e97a66e6
Daniel Ojeda <211@holbertonschool.com>
2018-09-26 23:47:39 +0000

    pop

---
+++ pop.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+/**
+ * pop - pops element from top of stack
+ * @stack: pointer to stack
+ * @linenum: line number
+ *
+ * Return: void
+ */
+void pop(stack_t **stack, unsigned int linenum)
+{
+	stack_t *temp;
+
+	if (stack == NULL || (*stack) == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack", linenum);
+		exit(EXIT_FAILURE);
+	}
+	temp = *stack;
+	(*stack) = (*stack)->next;
+	free(temp);
+}
