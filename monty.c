3b1b6f39ddc1a65acc6ec9c0ed322793ff262d8d
Daniel Ojeda <211@holbertonschool.com>
2018-09-26 23:22:13 +0000

    print to stderr

--- monty.c
+++ monty.c
@@ -17,14 +17,14 @@ int main(int argc, char **argv)
 
 	if (argc != 2)
 	{
-		printf("USAGE: monty file\n");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 
 	fp = fopen(argv[1], "r");
 	if (fp == NULL)
 	{
-		printf("Error: Can't open file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	while (getline(&buff, &len, fp) != -1)
