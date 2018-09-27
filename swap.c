acb5ca28cbd8c36666f2b5478c72eb3445dcbe21
Daniel Ojeda <211@holbertonschool.com>
2018-09-27 19:16:44 +0000

    add swap func

---
+++ swap.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+/**
+ * swap - swaps the top two elements of the stack
+ * @stack: pointer to stack
+ * @linenum: line number
+ *
+ * Return: void
+ */
+void swap(stack_t **stack, unsigned int linenum)
+{
+	stack_t *temp;
+	int val;
+
+	if (!stack | !(*stack) | !(*stack)->next)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", linenum);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = (*stack);
+	val = temp->n;
+	temp->n = temp->next->n;
+	temp->next->n = val;
+}
