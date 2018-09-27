435865a612f2ac2d814eec0bd810824af3eda10d
Daniel Ojeda <211@holbertonschool.com>
2018-09-27 19:56:34 +0000

    nop func that does nothing

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * nop - this function will do absolutely nothing
+ *
+ * @stack: pointer to stack
+ * @linenum: line number
+ * Return: void
+ */
+void nop(stack_t **stack, unsigned int linenum)
+{
+	(void) stack;
+	(void) linenum;
+}
