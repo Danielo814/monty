4e94bd3c749b99651e527a017fdebb327b4d90c7
Daniel Ojeda <211@holbertonschool.com>
2018-09-27 19:47:47 +0000

    syntax fix

--- add.c
+++ add.c
@@ -10,7 +10,7 @@ void add(stack_t **stack, unsigned int linenum)
 {
 	int val;
 
-	if (stack == NULL | (*stack) == NULL | (*stack)->next == NULL)
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", linenum);
 		exit(EXIT_FAILURE);
